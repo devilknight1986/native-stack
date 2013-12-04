569f53ced325ebe02be68f201a3c36398ae5f5bd
Wang Zhiyong <wang.zhiyong@rea-group.com>
2013-12-04 10:12:18 +0800

    Use linked list to save data.

--- NativeStack.c
+++ NativeStack.c
@@ -3,8 +3,31 @@
 #include <string.h>
 #include "NativeStack.h"
 
-char* elem = NULL;
-int lenght = 0;
+typedef struct elem_struct {
+    char* data;
+    int length;
+    struct elem_struct* next;
+} *elem;
+
+elem head = NULL;
+
+void addElemToFirst(char* data, int length) {
+    elem new_elem = malloc(sizeof(sizeof(struct elem_struct)));
+    new_elem->length=length;
+    new_elem->data=data;
+    new_elem->next=head;
+    head = new_elem;
+}
+
+void deleteFirstElem() {
+    if(head == NULL)
+        return;
+    elem new_head=head->next;
+    free(head->data);
+    free(head);
+    head = new_head;
+}
+
 /*
  * Class:     NativeStack
  * Method:    nativePush
@@ -14,12 +37,9 @@ JNIEXPORT void JNICALL Java_NativeStack_nativePush
 (JNIEnv *env, jobject self, jbyteArray data, jint len){
     jboolean isCopy = JNI_FALSE;
     jbyte* array = (*env) -> GetByteArrayElements(env, data, &isCopy);
-    free(elem);
-    elem = NULL;
-    lenght = 0;
-    elem = malloc(len * sizeof(char));
-    memcpy(elem, array, len);
-    lenght = len;
+    char* elem_data = (char*)malloc(len * sizeof(char));
+    memcpy(elem_data, array, len);
+    addElemToFirst(elem_data, len);
     (*env) -> ReleaseByteArrayElements(env, data, array, JNI_ABORT);
 };
 
@@ -30,9 +50,9 @@ JNIEXPORT void JNICALL Java_NativeStack_nativePush
  */
 JNIEXPORT jbyteArray JNICALL Java_NativeStack_nativeGet
 (JNIEnv *env, jobject self) {
-    if(elem == NULL) return NULL;
-    jbyteArray array = (*env)->NewByteArray(env, lenght);
-    (*env)->SetByteArrayRegion(env,array,0,lenght, (jbyte*)elem);
+    if(head == NULL) return NULL;
+    jbyteArray array = (*env)->NewByteArray(env, head->length);
+    (*env)->SetByteArrayRegion(env,array,0, head->length, (jbyte*)head->data);
     return array;
 };
 
@@ -43,7 +63,7 @@ JNIEXPORT jbyteArray JNICALL Java_NativeStack_nativeGet
  */
 JNIEXPORT void JNICALL Java_NativeStack_nativePop
 (JNIEnv *env, jobject self){
-    elem = NULL;
+    deleteFirstElem();
 };
 
 /*
@@ -53,7 +73,7 @@ JNIEXPORT void JNICALL Java_NativeStack_nativePop
  */
 JNIEXPORT jboolean JNICALL Java_NativeStack_nativeIsEmpty
 (JNIEnv *env, jobject self){
-    return elem == NULL;
+    return head == NULL;
 };
 
 
