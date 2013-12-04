29e87a1d1db84bc9657609782d744c8bfa4c3d1f
Wang Zhiyong <wang.zhiyong@rea-group.com>
2013-12-04 09:58:19 +0800

    Second commit.

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+JNI_LIB="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers"
 rm -rf *.class *.dylib
 javac NativeStack.java Main.java
 javah NativeStack
-gcc -dynamiclib -o libnative.dylib "-I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers" NativeStack.c -framework JavaVM
+gcc -dynamiclib -o libnative.dylib -I$JNI_LIB NativeStack.c -framework JavaVM
 java -Djava.library.path=. Main a bc def ghij klmno pqrstu vwxyz
